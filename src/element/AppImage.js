2d607498223183b479bcec8f7f827598e6e9b31a
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2022-01-10 13:56:21 +0700

    Update element Loading, LoadingProcess, AppImage

---
+++ src/element/AppImage.js
@@ -0,0 +1,104 @@
+import React, { PureComponent } from "react";
+import { View } from "react-native";
+import FastImage from "react-native-fast-image";
+import { grey300 } from "../const/Color";
+import { Loading } from "./Loading";
+
+class AppImage extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: true,
+      height: this.props.style.height
+    };
+  }
+
+  getResizeMode = resizeMode => {
+    switch (resizeMode) {
+      case "cover":
+        return FastImage.resizeMode.cover;
+      case "center":
+        return FastImage.resizeMode.center;
+      case "stretch":
+        return FastImage.resizeMode.stretch;
+      default:
+        return FastImage.resizeMode.contain;
+    }
+  };
+
+  render() {
+    const {
+      resizeMode = "contain",
+      style,
+      source,
+      defaultSource,
+      sizeSpinner
+    } = this.props;
+    const { loading } = this.state;
+    //Không có Uri:
+    if (source && source.uri === null) {
+      return (
+        <View
+          style={[
+            {
+              width: 86,
+              height: 86,
+              backgroundColor: grey300
+            },
+            style
+          ]}
+        />
+      );
+    }
+    //Loading tải ảnh:
+    if (loading) {
+      return (
+        <View>
+          <FastImage
+            defaultSource={defaultSource}
+            style={[{ width: 86, height: 86 }, style]}
+            source={source}
+            resizeMode={this.getResizeMode(resizeMode)}
+            onLoadEnd={() => {
+              this.setState({ loading: false });
+            }}
+            onError={() => {
+              this.setState({ loading: false });
+            }}
+          />
+          <Loading
+            sizeSpinner={sizeSpinner}
+            style={[
+              {
+                width: 86,
+                height: 86,
+                position: "absolute",
+                top: 0,
+                left: 0
+              },
+              style
+            ]}
+          />
+        </View>
+      );
+    }
+    //Ảnh cache hoặc đã tải xong:
+    return (
+      <FastImage
+        defaultSource={defaultSource}
+        style={[
+          {
+            width: 50,
+            height: 50,
+            overflow: "hidden"
+          },
+          style
+        ]}
+        source={source}
+        resizeMode={this.getResizeMode(resizeMode)}
+      />
+    );
+  }
+}
+export { AppImage };
+
---
+++ src/element/LoadingProcess.js
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Image } from "react-native";
+import * as Progress from "react-native-progress";
+import Icon from "react-native-vector-icons/FontAwesome";
+import { pink50 } from "../const/Color";
+import { SizeRpScreen } from "../resources/ResponsiveScreen";
+
+const LoadingProcess = props => {
+  let timeLoading = 0;
+  const processLoading = [15, 20, 30, 60, 70, 80, 100];
+  const [process, setStateProcess] = useState(0);
+  useEffect(() => {
+    processActions();
+    return () => {
+      clearTimeout(timeLoading);
+    };
+  }, []);
+  const processActions = () => {
+    const timeNextProcess = 200;
+    for (
+      let indexProcess = 0;
+      indexProcess <= processLoading.length - 1;
+      indexProcess++
+    ) {
+      (function(index) {
+        timeLoading = setTimeout(function() {
+          setStateProcess(processLoading[index] / 100);
+          if (
+            index == processLoading.length - 1 &&
+            props &&
+            props.stopLoadingStartAppIntro
+          ) {
+            props.stopLoadingStartAppIntro();
+          }
+        }, timeNextProcess * index);
+      })(indexProcess);
+    }
+  };
+  const renderIconApp = () => {
+    return <Icon name={"grav"} size={50} color={"red"} />;
+  };
+  renderLoading = () => {
+    const { typeLoading = "Bar" } = props;
+    switch (typeLoading) {
+      case "Bar":
+        return (
+          <Progress.Bar
+            animated={true}
+            progress={process}
+            width={300}
+            useNativeDriver={true}
+            height={10}
+            animationConfig={{ bounciness: 2 }}
+            animationType={"spring"}
+          />
+        );
+      case "Pie":
+        return <Progress.Pie progress={process} size={250} />;
+      case "Circle":
+        return <Progress.Circle size={50} indeterminate={true} />;
+      case "CircleSnail":
+        return <Progress.CircleSnail color={["red", "green", "blue"]} />;
+    }
+  };
+
+  const renderGifFrog = () => {
+    return (
+      <Image
+        source={require("../images/Frog.gif")}
+        style={{ width: 100, height: 100 }}
+      />
+    );
+  };
+
+  return (
+    <View style={styles.container}>
+      {renderIconApp()}
+      {renderGifFrog()}
+      {renderLoading()}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: SizeRpScreen.device_height,
+    width: SizeRpScreen.device_width,
+    backgroundColor: pink50,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+export default LoadingProcess;
