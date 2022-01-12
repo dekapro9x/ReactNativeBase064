dcf274d877606619ed59fafd9c21ecc8e71db471
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2022-01-12 18:01:31 +0700

    Update Login screen, ts config

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -1,34 +1,69 @@
-import React from 'react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
-import { keyNavigation } from '../../navigation/KeyNavigations';
-
-export default function Login({navigation, router}) {
+import React, {useContext} from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import LinearGradient from "react-native-linear-gradient";
+import { AppContainer } from "src/element/AppContainer";
+import { ContextContainer } from "../../context/AppContext";
+import { AppContainer } from "../../element/AppContainer";
+import { keyNavigation } from "../../navigation/KeyNavigations";
+export default function Login({ navigation, router }) {
+  const {colorApp}  = useContext(ContextContainer);
   const navigateHome = () => {
     navigation.navigate(keyNavigation.HOME);
   };
+  const renderContent = () => {
+    return (
+      <View style={styles.containerContent}>
+        <Text>Basic JS Screen</Text>
+      </View>
+    );
+  };
+
+  // return (
+  //   <View style={styles.container}>
+  //     <LinearGradient
+  //       colors={["#481E34", "#16192B"]}
+  //       end={{ x: 1, y: 1 }}
+  //       start={{ x: 0, y: 0 }}
+  //       style={styles.container}
+  //     >
+  //       <TouchableOpacity
+  //         styles={{
+  //           height: 45,
+  //           width: 250,
+  //           boderWidth: 1,
+  //           boderRadius: 10,
+  //         }}
+  //         onPress={navigateHome}
+  //       >
+  //         <Text>LOGIN SCREEN</Text>
+  //       </TouchableOpacity>
+  //     </LinearGradient>
+  //   </View>
+  // );
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        styles={{
-          height: 45,
-          width: 250,
-          boderWidth: 1,
-          boderRadius: 10,
-          backgroundColor: 'green',
-        }}
-        onPress={navigateHome}>
-        <Text>LOGIN SCREEN</Text>
-      </TouchableOpacity>
-    </View>
+    <AppContainer
+      nameScreen={""}
+      goBackScreen={false}
+      flexWrapHeader
+      style={[styles.container, { backgroundColor: colorApp.backgroundColor }]}
+    >
+      {renderContent()}
+    </AppContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor:'blue'
+    alignItems: "center",
+    justifyContent: "center",
   },
-  bnt_mid: {},
+  containerContent: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  
+
+  bnt_mid: {}
 });
