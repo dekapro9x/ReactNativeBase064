ef742b66cb9dbda3e90d2da3a75b092c364ac1de
Trần Xuân Nam <30828722+dekapro9x@users.noreply.github.com>
2022-01-14 14:28:46 +0700

    Update Login screen

--- src/screen/Login/index.js
+++ src/screen/Login/index.js
@@ -1,19 +1,51 @@
 import { AppTextInput } from "@element/AppTextInput";
+import { DebounceButton } from "@element/DebounceButton";
 import { SizeRpScreen } from "@resources/ResponsiveScreen";
 import { BEOTRAN_LOGGER } from "@util/Loger";
-import React from "react";
-import { StyleSheet, View } from "react-native";
+import React, { useRef } from "react";
+import { Alert, StyleSheet, View } from "react-native";
 import LinearGradient from "react-native-linear-gradient";
 import { AppContainer } from "../../element/AppContainer";
 import { keyNavigation } from "../../navigation/KeyNavigations";
 
 export default function Login({ navigation, router }) {
+  const useName = useRef("");
+  const passWord = useRef("");
+
   const navigateHome = () => {
-    navigation.navigate(keyNavigation.HOME);
+    navigation.replace(keyNavigation.HOME);
   };
 
   const onChangeText = (keyState, value) => {
-    BEOTRAN_LOGGER(keyState, value);
+    switch (keyState) {
+      case "UserName":
+        useName.current = value;
+        break;
+      case "Password":
+        passWord.current = value;
+        break;
+    }
+  };
+
+  const pressLogin = () => {
+    if (validateAccount()) {
+      navigateHome();
+    }
+  };
+
+  const validateAccount = () => {
+    BEOTRAN_LOGGER(useName.current, passWord.current);
+    if (!useName.current) {
+      Alert.alert("Đăng nhập không chính xác","Tài khoản là bắt buộc");
+      return false;
+    }
+    if (!passWord.current) {
+      Alert.alert("Đăng nhập không chính xác","Mật khẩu là bắt buộc");
+      return false;
+    }
+    if (useName.current && passWord.current) {
+      return true;
+    }
   };
 
   const renderContent = () => {
@@ -42,6 +74,24 @@ export default function Login({ navigation, router }) {
             onChangeText={onChangeText}
           />
         </View>
+        <DebounceButton
+          useDelay={true}
+          onPress={pressLogin}
+          loadingColor="#FFFFFF"
+          title={"Đăng nhập"}
+          textStyle={{
+            color: "#FFFFFF",
+            fontSize: SizeRpScreen.H5 * 1.2,
+            fontWeight: "bold",
+            textAlign: "center"
+          }}
+          style={{
+            backgroundColor: "#06B050",
+            justifyContent: "center",
+            alignItems: "center",
+            alignSelf: "center"
+          }}
+        />
       </LinearGradient>
     );
   };
