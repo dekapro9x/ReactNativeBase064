4364319ddf56ed4e5521def3a9ab94e953468229
dekapro9x <dekapro9x@gmail.com>
2022-09-29 16:24:30 +0700

    Update get size img

--- src/element/AppImageRenderSize.tsx
+++ src/element/AppImageRenderSize.tsx
@@ -1,8 +1,9 @@
 
 import { SizeRpScreen } from '@resources/ResponsiveScreen';
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Image } from "react-native";
 import FastImage from "react-native-fast-image";
-import ImageResizer from 'react-native-image-resizer';
+import ImageResizer from 'react-native-image-resizer'
 
 interface Props {
     route: object;
@@ -33,6 +34,7 @@ const ImagesRenderSize = (props: Props) => {
                 // response.path is the path of the new image
                 // response.name is the name of the new image with the extension
                 // response.size is the size of the new image
+                console.log("createResizedImage", response);
                 // setStateUriImgRender(response.uri);
                 setStateHeightImgRenderSize(response.height * scaleHandle);
                 setStateWidthImgRenderSize(response.width * scaleHandle);
@@ -41,6 +43,24 @@ const ImagesRenderSize = (props: Props) => {
                 // Oops, something went wrong. Check that the filename is correct and
                 // inspect err to get more details.
             });
+    }
+
+    const setHeightScaleScreenImg = () => {
+        Image.getSize(uriImgRender, (width, height) => {
+            console.log("Chiều cao ảnh:", height);
+            console.log("Chiều cao màn hình", SizeRpScreen.device_height);
+            const heightConvert = (height / SizeRpScreen.device_height) * scaleHandle / 100;
+            console.log("Chiều cao convert:", heightConvert);
+
+        });
+    }
+    const setWidthScaleScreenImg = () => {
+        Image.getSize(uriImgRender, (width, height) => {
+            console.log("Chiều rộng ảnh:", width);
+            console.log("Chiều rộng màn hình", SizeRpScreen.device_width);
+            const widthConvert = width / SizeRpScreen.device_width;
+            console.log("Chiều rộng convert", widthConvert);
+        });
     }
 
     return (
@@ -52,4 +72,4 @@ const ImagesRenderSize = (props: Props) => {
     )
 }
 
-export { ImagesRenderSize };
+export { ImagesRenderSize }
