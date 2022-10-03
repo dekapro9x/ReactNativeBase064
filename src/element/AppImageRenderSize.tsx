5da02790f901123e866f8d5b8ee23da18124df37
dekapro9x <dekapro9x@gmail.com>
2022-10-03 14:17:29 +0700

    Update images render size

--- src/element/AppImageRenderSize.tsx
+++ src/element/AppImageRenderSize.tsx
@@ -1,9 +1,8 @@
 
 import { SizeRpScreen } from '@resources/ResponsiveScreen';
-import React, { useState, useEffect } from 'react';
-import { Image } from "react-native";
+import React, { useEffect, useState } from 'react';
 import FastImage from "react-native-fast-image";
-import ImageResizer from 'react-native-image-resizer'
+import ImageResizer from 'react-native-image-resizer';
 
 interface Props {
     route: object;
@@ -14,7 +13,6 @@ interface Props {
 
 const ImagesRenderSize = (props: Props) => {
     const { uriImgRenderSize, scaleConvert } = props;
-    const [uriImgRender, setStateUriImgRender] = useState(uriImgRenderSize);
     const scaleHandle = scaleConvert || 1;
     const [heightImgRenderSize, setStateHeightImgRenderSize] = useState(0);
     const [widthImgRenderSize, setStateWidthImgRenderSize] = useState(0);
@@ -27,14 +25,13 @@ const ImagesRenderSize = (props: Props) => {
         const maxWidth = SizeRpScreen.device_width;
         const maxHeight = SizeRpScreen.device_height;
         const compressFormat = "PNG";
-        const quality = 1;
-        ImageResizer.createResizedImage(uriImgRender, maxWidth, maxHeight, compressFormat, quality)
+        const quality = 100;
+        ImageResizer.createResizedImage(uriImgRenderSize, maxWidth, maxHeight, compressFormat, quality)
             .then(response => {
                 // response.uri is the URI of the new image that can now be displayed, uploaded...
                 // response.path is the path of the new image
                 // response.name is the name of the new image with the extension
                 // response.size is the size of the new image
-                console.log("createResizedImage", response);
                 // setStateUriImgRender(response.uri);
                 setStateHeightImgRenderSize(response.height * scaleHandle);
                 setStateWidthImgRenderSize(response.width * scaleHandle);
@@ -44,32 +41,13 @@ const ImagesRenderSize = (props: Props) => {
                 // inspect err to get more details.
             });
     }
-
-    const setHeightScaleScreenImg = () => {
-        Image.getSize(uriImgRender, (width, height) => {
-            console.log("Chiều cao ảnh:", height);
-            console.log("Chiều cao màn hình", SizeRpScreen.device_height);
-            const heightConvert = (height / SizeRpScreen.device_height) * scaleHandle / 100;
-            console.log("Chiều cao convert:", heightConvert);
-
-        });
-    }
-    const setWidthScaleScreenImg = () => {
-        Image.getSize(uriImgRender, (width, height) => {
-            console.log("Chiều rộng ảnh:", width);
-            console.log("Chiều rộng màn hình", SizeRpScreen.device_width);
-            const widthConvert = width / SizeRpScreen.device_width;
-            console.log("Chiều rộng convert", widthConvert);
-        });
-    }
-
     return (
         <FastImage
             resizeMode={"contain"}
             style={{ height: heightImgRenderSize, width: widthImgRenderSize }}
-            source={{ uri: uriImgRender }}>
+            source={{ uri: uriImgRenderSize }}>
         </FastImage>
     )
 }
 
-export { ImagesRenderSize }
+export { ImagesRenderSize };
